0ba091c1f2bec63eac94f9acf1c9275d692dcba2
Eric Slutz <eric.slutz@icloud.com>
2023-05-24 13:45:29 -0400

    created layout and functionality

--- app/src/main/java/com/zybooks/thebanddatabase/ListFragment.java
+++ app/src/main/java/com/zybooks/thebanddatabase/ListFragment.java
@@ -1,64 +1,91 @@
 package com.zybooks.thebanddatabase;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.List;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ListFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ListFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ListFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ListFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ListFragment newInstance(String param1, String param2) {
-        ListFragment fragment = new ListFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_list, container, false);
+
+        // Click listener for the RecyclerView
+        View.OnClickListener onClickListener = itemView -> {
+
+            // Create fragment arguments containing the selected band ID
+            int selectedBandId = (int) itemView.getTag();
+            Bundle args = new Bundle();
+            args.putInt(DetailFragment.ARG_BAND_ID, selectedBandId);
+
+            // Replace list with details
+            Navigation.findNavController(itemView).navigate(R.id.show_item_detail, args);
+        };
+
+        // Send bands to RecyclerView
+        RecyclerView recyclerView = rootView.findViewById(R.id.band_list);
+        List<Band> bands = BandRepository.getInstance(requireContext()).getBands();
+        recyclerView.setAdapter(new BandAdapter(bands, onClickListener));
+
+        // Add a divider line between items in a RecyclerView
+        DividerItemDecoration divider = new DividerItemDecoration(recyclerView.getContext(),
+                DividerItemDecoration.VERTICAL);
+        recyclerView.addItemDecoration(divider);
+
+        return rootView;
+    }
+
+    private class BandAdapter extends RecyclerView.Adapter<BandHolder> {
+
+        private final List<Band> mBands;
+        private final View.OnClickListener mOnClickListener;
+
+        public BandAdapter(List<Band> bands, View.OnClickListener onClickListener) {
+            mBands = bands;
+            mOnClickListener = onClickListener;
+        }
+
+        @NonNull
+        @Override
+        public BandHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+            return new BandHolder(layoutInflater, parent);
+        }
+
+        @Override
+        public void onBindViewHolder(BandHolder holder, int position) {
+            Band band = mBands.get(position);
+            holder.bind(band);
+            holder.itemView.setTag(band.getId());
+            holder.itemView.setOnClickListener(mOnClickListener);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mBands.size();
+        }
+    }
+
+    private static class BandHolder extends RecyclerView.ViewHolder {
+
+        private final TextView mNameTextView;
+
+        public BandHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater.inflate(R.layout.list_item_band, parent, false));
+            mNameTextView = itemView.findViewById(R.id.band_name);
+        }
+
+        public void bind(Band band) {
+            mNameTextView.setText(band.getName());
+        }
     }
 }
