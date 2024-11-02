c2e90592a512d4b8aee12e81b6156e6997523e01
sofieny <sophie.ying.nie@gmail.com>
2024-11-02 17:58:52 -0500

    ny:all xml + question.kt + subject.kt

---
+++ app/src/main/java/com/cs407/memora/model/Question.kt
@@ -0,0 +1,25 @@
+//6.4, 6.5
+package com.cs407.memora.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.ForeignKey.CASCADE
+import androidx.room.PrimaryKey
+
+@Entity(foreignKeys = [
+    ForeignKey(entity = Subject::class,
+        parentColumns = ["id"],
+        childColumns = ["subject_id"],
+        onDelete = CASCADE)
+])
+data class Question(
+    @PrimaryKey(autoGenerate = true)
+    var id: Long = 0,
+
+    var text: String = "",
+    var answer: String = "",
+
+    @ColumnInfo(name = "subject_id")
+    var subjectId: Long = 0) {
+}
---
+++ app/src/main/java/com/cs407/memora/model/Subject.kt
@@ -0,0 +1,19 @@
+//6.4, 6.5
+package com.cs407.memora.model
+
+import androidx.annotation.NonNull
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Subject(
+    @PrimaryKey(autoGenerate = true)
+    var id: Long = 0,
+
+    @NonNull
+    var text: String,
+
+    @ColumnInfo(name = "updated")
+    var updateTime: Long = System.currentTimeMillis()) {
+}
