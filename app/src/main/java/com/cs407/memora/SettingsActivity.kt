014f9cdb39f5f31374fb0209c241df0f434cf799
Yee <yeewei.math@gmail.com>
2024-11-16 15:41:55 -0600

    Correct setting theme

--- app/src/main/java/com/cs407/memora/SettingsActivity.kt
+++ app/src/main/java/com/cs407/memora/SettingsActivity.kt
@@ -2,7 +2,9 @@ package com.cs407.memora
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.SwitchPreferenceCompat
 
 class SettingsActivity : AppCompatActivity() {
 
@@ -21,6 +23,18 @@ class SettingsActivity : AppCompatActivity() {
     class SettingsFragment : PreferenceFragmentCompat() {
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
+
+            val themePref: SwitchPreferenceCompat? = findPreference("dark_theme")
+            themePref?.setOnPreferenceChangeListener { preference, newValue ->
+
+                // Turn on or off night mode
+                if (newValue == true) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                } else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                }
+                true
+            }
         }
     }
 }
